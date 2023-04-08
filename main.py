49cd1f46cc1a79e0650ba0037485162c4a9898be
Isaiah Nichols <isaiah.nichols180@gmail.com>
2023-04-07 21:31:04 -0700

    major changes: got input from user, interaction with api, and output both text and audio.

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 # Project Name to be Determined
-import os
-import openai
-from gtts import gTTS
 
 def main():
-    text = ""
-    volume = 0.05
+    import os
+    import openai
+    from gtts import gTTS
 
-    while text != "stop":
-        lang = "en"
-        text = input("Enter Words to be Spoken: ")
+    with open("ignore/key.txt") as f:
+        key = f.readline()
+        key = key.strip()
 
-        audio = gTTS(text, lang=lang, slow=False)
-        audio.save("audio.mp3")
+    openai.api_key = key
 
-        os.system(f"afplay -v {volume} assets/audio.mp3")
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+    
+    running = 1
+    while running:
+        message = input("You: ") # need to change input source... using this for testing
+
+        if message:
+            # Interacting with API
+            messages.append({"role": "user", "content": message})
+
+            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+            response_text = response.choices[0].message.content
+            
+            # Terminal output
+            print(f"Chat: {response_text}")
+
+            # Audio output
+            lang = "en"
+            volume = 0.05
+            audio = gTTS(response_text, lang=lang, slow=False)
+            audio.save("ignore/audio.mp3")
+
+            os.system(f"afplay -v {volume} ignore/audio.mp3")
+        else:
+            running = 0
 
 if __name__ == "__main__":
     main()
